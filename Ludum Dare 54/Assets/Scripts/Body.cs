78890b9f269fb1e8739a3c4d9836259fdd826963
blaerzhang <zhangyueyang0022@126.com>
2023-10-02 07:01:47 -0400

    Change Star System to Manual;

--- Ludum Dare 54/Assets/Scripts/Body.cs
+++ Ludum Dare 54/Assets/Scripts/Body.cs
@@ -1,14 +1,17 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Body : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.DORotate(new Vector3(0, 0, 360) * (Random.Range(0, 2) * 2 - 1), Random.Range(30f, 90f),
+            RotateMode.FastBeyond360);
     }
 
     // Update is called once per frame
