9f8082d16ecfc2e3859082a826fb476ff928e677
Ziggy-dst <542335894@qq.com>
2023-10-02 03:20:29 -0400

    Inventory Setup, finished inventory grid snapping

--- Ludum Dare 54/Assets/Scripts/GridSystem/DraggableItem.cs
+++ Ludum Dare 54/Assets/Scripts/GridSystem/DraggableItem.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class DraggableItem : DraggableObject
 {
     private bool inInventory = false;
+    private Transform inventory;
+    // private List<>
 
     protected override void OnMouseDown()
     {
@@ -18,12 +20,16 @@ public class DraggableItem : DraggableObject
     protected override void OnMouseUp()
     {
         isDragging = false;
-        // if (GetComponent<SpriteRenderer>() != null) GetComponent<SpriteRenderer>().sortingOrder = 1002;
+        // if (GetComponent<SpriteRenderer>() != null) GetComponent<SpriteRenderer>().sortingOrder = 1001;
 
         if (inInventory)
         {
             // if (GetComponent<SpriteRenderer>() != null) GetComponent<SpriteRenderer>().sortingOrder = 2;
-            if (canDrop) transform.position = Snap(newPosition);
+            if (canDrop)
+            {
+                if (inventory != null) transform.parent = inventory;
+                transform.localPosition = Snap(newPosition);
+            }
             else
             {
                 transform.position = originalPosition;
@@ -31,6 +37,10 @@ public class DraggableItem : DraggableObject
                 inViewport = true;
                 canDrop = true;
             }
+        }
+        else
+        {
+            Destroy(gameObject);
         }
     }
 
@@ -46,8 +56,9 @@ public class DraggableItem : DraggableObject
             inViewport = true;
         }
 
-        if (col.tag.Equals("PlayableUI") & col.name.Equals("Inventory"))
+        if (col.tag.Equals("InventoryShape"))
         {
+            inventory = col.transform.parent;
             inInventory = true;
         }
 
@@ -65,7 +76,7 @@ public class DraggableItem : DraggableObject
             inViewport = true;
         }
 
-        if (col.tag.Equals("PlayableUI") & col.name.Equals("Inventory"))
+        if (col.tag.Equals("InventoryShape"))
         {
             inInventory = true;
         }
@@ -84,7 +95,7 @@ public class DraggableItem : DraggableObject
             inViewport = false;
         }
 
-        if (col.tag.Equals("PlayableUI") & col.name.Equals("Inventory"))
+        if (col.tag.Equals("InventoryShape"))
         {
             inInventory = false;
         }
@@ -98,8 +109,10 @@ public class DraggableItem : DraggableObject
 
     protected override Vector2 Snap(Vector2 position)
     {
-        float adjustedX = position.x - objectSize.x / 2f;
-        float adjustedY = position.y - objectSize.y / 2f + 0.5f;
+        Vector2 localPosition = transform.parent.InverseTransformPoint(position);
+
+        float adjustedX = localPosition.x - objectSize.x / 2f ;
+        float adjustedY = localPosition.y - objectSize.y / 2f + 0.5f;
 
         float x = Mathf.Round(adjustedX / cellSize) * cellSize + objectSize.x / 2f;
         float y = Mathf.Round(adjustedY / cellSize) * cellSize + objectSize.y / 2f - 0.5f;
